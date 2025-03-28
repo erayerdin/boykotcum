871a2534e0091923a8059b5fa87193320c42e859
Eray Erdin <eraygezer.94@gmail.com>
2025-03-28 22:17:17 +0300

    remove headers

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -19,7 +19,7 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{ headerShown: false }}>
       <Stack.Screen name="index" options={{ title: "Home" }} redirect />
       <Stack.Screen name="camera" options={{ title: "Camera" }} />
     </Stack>
