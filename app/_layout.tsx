4447306536c06ccdd36544b942fb2b80ee2897fe
Eray Erdin <eraygezer.94@gmail.com>
2025-03-28 22:16:51 +0300

    disable back navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -20,7 +20,8 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
+      <Stack.Screen name="index" options={{ title: "Home" }} redirect />
+      <Stack.Screen name="camera" options={{ title: "Camera" }} />
     </Stack>
   );
 }
