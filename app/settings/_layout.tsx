869262cc706446a69d12ecc26c729d8e3548a075
Eray Erdin <eraygezer.94@gmail.com>
2025-04-01 10:02:11 +0300

    rename boykotsepeti to Boykotçum

--- app/settings/_layout.tsx
+++ app/settings/_layout.tsx
@@ -1,19 +1,19 @@
 // Copyright (C) 2025 Eray Erdin
 //
-// This file is part of boykotsepeti.
+// This file is part of Boykotçum.
 //
-// boykotsepeti is free software: you can redistribute it and/or modify
+// Boykotçum is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 2 of the License, or
 // (at your option) any later version.
 //
-// boykotsepeti is distributed in the hope that it will be useful,
+// Boykotçum is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with boykotsepeti.  If not, see <https://www.gnu.org/licenses/>.
+// along with Boykotçum.  If not, see <https://www.gnu.org/licenses/>.
 
 import { Stack } from "expo-router";
 import React from "react";
