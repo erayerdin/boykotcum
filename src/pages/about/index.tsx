71cfc865176c674ab4da2149d66e8829196ffc01
Eray Erdin <eraygezer.94@gmail.com>
2025-04-22 02:17:19 +0300

    design about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -16,15 +16,68 @@
 // along with Boykotçum.  If not, see <https://www.gnu.org/licenses/>.
 
 import { Button } from "@/components/ui/button";
+import { SiDiscord, SiGithub, SiGnu } from "@icons-pack/react-simple-icons";
 import { useNavigate } from "react-router";
 
 const AboutPage = () => {
   // navigate back
   const navigate = useNavigate();
 
+  const links = [
+    {
+      title: "Discord'a Katıl",
+      description:
+        "Fikrini beyan et. Boykot etmek için yeni bir marka veya firma öner.",
+      url: "https://discord.gg/6KpMXts6eu",
+      icon: <SiDiscord size={24} />,
+      color: "bg-[#5865F2]",
+    },
+    {
+      title: "Github'da Kod Ekle ve Hata Bildir",
+      description: "Kodları incele. Hata bildir. Katkıda bulun.",
+      url: "https://github.com/erayerdin/boykotcum",
+      icon: <SiGithub size={24} />,
+      color: "bg-[#181717]",
+    },
+    {
+      title: "GNU GPL ile bu programın ne yaptığını bil",
+      description:
+        "Bu program GNU GPL v2.0 lisansı ile lisanslanmıştır. Bu programın ne yaptığını kodları inceleyerek görebilirsiniz.",
+      url: "https://www.gnu.org/licenses/old-licenses/gpl-2.0.html",
+      icon: <SiGnu size={24} />,
+      color: "bg-[#A42E2B]",
+    },
+  ];
+
   return (
     <div className="flex flex-col justify-between h-screen max-h-screen p-2">
-      <div className="grow overflow-y-auto">Content</div>
+      <div className="flex flex-col gap-4 grow overflow-y-auto">
+        {/* Main Card */}
+        <div className="flex flex-col gap-2 bg-gradient-to-br from-green-600 to-green-400 p-8 rounded-lg shadow text-white text-center">
+          <p className="text-2xl font-extrabold">Boykotçum</p>
+          <p className="text-sm italic">
+            &quot;Ne almayacağını sen düşünme.&quot;
+          </p>
+        </div>
+
+        {/* Links */}
+        <div className="flex flex-col gap-2">
+          {links.map(({ title, description, url, icon, color }) => (
+            <a
+              key={url}
+              className={`flex flex-col gap-2 rounded-md ${color} text-white p-2`}
+              href={url}
+            >
+              <div className="flex items-center gap-2 justify-center text-center">
+                {icon}
+                <p>{title}</p>
+              </div>
+              <hr className="border-white" />
+              <p className="text-xs text-center">{description}</p>
+            </a>
+          ))}
+        </div>
+      </div>
       <Button type="button" onClick={() => navigate(-1)}>
         Geri Dön
       </Button>
