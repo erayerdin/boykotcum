38c7c9fbf6e9d96fa2b49f7c266e81f99b852cc4
Eray Erdin <eraygezer.94@gmail.com>
2025-04-21 14:00:50 +0300

    configure pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,47 @@ import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), VitePWA({ registerType: "autoUpdate" })],
+export default defineConfig(({ mode }) => ({
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: ["development", "e2e"].includes(mode),
+        type: ["module", "classic"].includes(mode) ? "module" : "classic",
+        navigateFallback: "index.html",
+      },
+      manifest: {
+        id: "com.erayerdin.boykotcum",
+        name: "Boykotçum",
+        short_name: "Boykotçum",
+        description: "Ne almayacağını sen düşünme.",
+        // icons,
+        lang: "tr-TR",
+        launch_handler: {
+          client_mode: "auto",
+        },
+        orientation: "any",
+        screenshots: [],
+        // theme_color: tailwindConfig.theme.extend.colors.brand,
+        categories: ["shopping"],
+        dir: "ltr",
+        prefer_related_applications: true,
+        related_applications: [
+          {
+            platform: "play",
+            id: "com.erayerdin.boykotcum",
+            url: "https://play.google.com/store/apps/details?id=com.erayerdin.boykotcum",
+          },
+        ],
+        scope_extensions: [],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
