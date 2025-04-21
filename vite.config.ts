6255bcb514856d18cfef4b83b5e5300867e949a3
Eray Erdin <eraygezer.94@gmail.com>
2025-04-22 01:48:47 +0300

    remove console logs from production

--- vite.config.ts
+++ vite.config.ts
@@ -44,6 +44,13 @@ export default defineConfig(({ mode }) => ({
       },
     }),
   ],
+  esbuild: {
+    drop: mode === "production" ? ["debugger"] : [],
+    pure:
+      mode === "production"
+        ? ["console.log", "console.debug", "console.trace"]
+        : [],
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
