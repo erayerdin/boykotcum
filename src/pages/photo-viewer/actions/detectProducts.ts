e1231c11ad77b851cde69b7a9b126464494e58af
Eray Erdin <eraygezer.94@gmail.com>
2025-04-22 10:59:52 +0300

    move out MOCK_PRODUCTS

--- src/pages/photo-viewer/actions/detectProducts.ts
+++ src/pages/photo-viewer/actions/detectProducts.ts
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Boykotçum.  If not, see <https://www.gnu.org/licenses/>.
 
+import { MOCK_PRODUCTS } from "@/constants";
 import productDetectionPrompt from "@/prompts/productDetectionPrompt";
 import { Product } from "@/types";
 import sleepAsync from "@/utils/sleepAsync";
@@ -24,97 +25,6 @@ import {
   GoogleGenAI,
   Type,
 } from "@google/genai";
-
-const MOCK_PRODUCTS: Product[] = [
-  {
-    name: "Ülker",
-    description: "",
-    links: [
-      {
-        type: "text",
-        platform: "twitter",
-        link: "https://twitter.com/sample-post",
-        title: "Sample Twitter Post",
-        description: "A sample description for a Twitter post.",
-      },
-      {
-        type: "text",
-        platform: "twitter-webarchive",
-        link: "https://twitter.com/sample-post",
-        title: "Sample Twitter Post",
-        description: "A sample description for a Twitter post.",
-      },
-      {
-        type: "text",
-        platform: "other",
-        link: "https://examplesite.com/sample-post",
-        title: "Sample Post",
-        description: "A sample description for a generic post.",
-      },
-      {
-        type: "image",
-        platform: "instagram",
-        link: "https://instagram.com/sample-image",
-        title: "Sample Instagram Image",
-        description: "A sample description for an Instagram image.",
-      },
-      {
-        type: "image",
-        platform: "instagram-webarchive",
-        link: "https://instagram.com/sample-image",
-        title: "Sample Instagram Image",
-        description: "A sample description for an Instagram image.",
-      },
-      {
-        type: "image",
-        platform: "imgbb",
-        link: "https://imgbb.com/sample-image",
-        title: "Sample ImgBB Image",
-        description: "A sample description for an ImgBB image.",
-      },
-      {
-        type: "image",
-        platform: "other",
-        link: "https://examplesite.com/sample-image",
-        title: "Sample Image Link",
-        description: "A sample description for a generic image link.",
-      },
-      {
-        type: "video",
-        platform: "youtube",
-        link: "https://youtube.com/sample-video",
-        title: "Sample YouTube Video",
-        description: "A sample description for a YouTube video.",
-      },
-      {
-        type: "video",
-        platform: "dailymotion",
-        link: "https://dailymotion.com/sample-video",
-        title: "Sample Dailymotion Video",
-        description: "A sample description for a Dailymotion video.",
-      },
-      {
-        type: "video",
-        platform: "streamable",
-        link: "https://streamable.com/sample-video",
-        title: "Sample Streamable Video",
-        description: "A sample description for a Streamable video.",
-      },
-      {
-        type: "video",
-        platform: "other",
-        link: "https://examplesite.com/sample-video",
-        title: "Sample Video Link",
-        description: "A sample description for a generic video link.",
-      },
-    ],
-  },
-  {
-    name: "BİM",
-    description: "",
-    links: [],
-  },
-];
 
 type ListProductsParams = {
   ai: GoogleGenAI;
