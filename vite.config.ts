6400fdaa27d1d3b1221055ab775c64166dbb9bc6
Eray Erdin <eraygezer.94@gmail.com>
2025-04-24 15:40:43 +0300

    init sentry for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -14,6 +15,11 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     tailwindcss(),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "eray-erdin",
+      project: "boykotcum",
+    }),
     VitePWA({
       registerType: "autoUpdate",
       devOptions: {
